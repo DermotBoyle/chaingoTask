b602eec8b41d0c32ac4e2092bd8c32c077fdce7a
DermotBoyle <dermotboyled@gmail.com>
2019-06-26 16:08:26 +0200

    scores consecutive strikes except frame 10

--- exercise.js
+++ exercise.js
@@ -10,49 +10,51 @@ var NewGame = new Game();
 
 //values
 var nil = 0;
-var x = 10,
-  X = 10;
+var x = 10;
 
 //input
 NewGame.score = [
   [10, 0],
   [10, 0],
-  [8, 0],
-  [7, 2],
-  [6, 1],
-  [4, 3],
-  [5, 3],
   [10, 0],
-  [2, 0],
-  [6, 2, 0]
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [7, 2],
+  [3, 3, 0]
 ];
 
 // function
 const basicBowlingCalculation = () => {
   for (let i = 0; i < NewGame.score.length && NewGame.frames < 10; i++) {
-    // if its a spare
-    if (NewGame.score[i][0] + NewGame.score[i][1] == 10) {
-      NewGame.result +=
-        NewGame.score[i][0] + NewGame.score[i][1] + NewGame.score[i + 1][0];
-      NewGame.frames++;
-    } else if (NewGame.score[i][0] == 10 && NewGame.score[i + 1][0] !== 10) {
-      // for a strike
+    // if its a strike with the next score not a strike
+    if (NewGame.score[i][0] == 10 && NewGame.score[i + 1][0] !== 10) {
       NewGame.result +=
         NewGame.score[i][0] + NewGame.score[i + 1][0] + NewGame.score[i + 1][1];
+      NewGame.frames++;
+      // if its a strike and the next score is also a strike
     } else if (NewGame.score[i][0] == 10 && NewGame.score[i + 1][0] == 10) {
       NewGame.result +=
         NewGame.score[i][0] + NewGame.score[i + 1][0] + NewGame.score[i + 2][0];
+      NewGame.frames++;
+      // if its a spare
+    } else if (NewGame.score[i][0] + NewGame.score[i][1] === 10) {
+      NewGame.result +=
+        NewGame.score[i][0] + NewGame.score[i][1] + NewGame.score[i + 1][0];
+      NewGame.frames++;
     } else if (NewGame.score[i][0] + NewGame.score[i][1] !== 10) {
       // adds up the other scores
       NewGame.result += NewGame.score[i][0] + NewGame.score[i][1];
       NewGame.frames++;
     }
 
-    //console.log(NewGame.result);
-    //console.log(NewGame.frames);
+    console.log(NewGame.result);
+    console.log(NewGame.frames);
   }
-  NewGame.result += NewGame.score[9][0] + NewGame.score[9][1];
-  console.log(NewGame.result);
-  // console.log(NewGame.frames);
+  // NewGame.result += NewGame.score[9][0] + NewGame.score[9][1];
+  //console.log(NewGame.result);
+  //console.log(NewGame.frames);
 };
 basicBowlingCalculation();
