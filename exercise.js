7d863d27a09e5dbb9c869fb80f7183859b5f01bd
DermotBoyle <dermotboyled@gmail.com>
2019-06-26 16:52:45 +0200

    bowling game function appears to work - needs tests

--- exercise.js
+++ exercise.js
@@ -23,14 +23,24 @@ NewGame.score = [
   [10, 0],
   [10, 0],
   [10, 0],
-  [10, 10, 10]
+  [10, 6, 4]
 ];
 
 // function
 const basicBowlingCalculation = () => {
   for (let i = 0; i < NewGame.score.length && NewGame.frames < 10; i++) {
+    // if its a spare on the 10th frame
+    if (
+      NewGame.score[i][0] + NewGame.score[i][1] === 10 &&
+      NewGame.frames == 9
+    ) {
+      NewGame.frames++;
+      NewGame.result +=
+        NewGame.score[i][0] + NewGame.score[i][1] + NewGame.score[i][2];
+    }
+
     // if its the the 9th frame with a strike
-    if (NewGame.score[i][0] == 10 && NewGame.frames == 8) {
+    else if (NewGame.score[i][0] == 10 && NewGame.frames == 8) {
       NewGame.result +=
         NewGame.score[i][0] + NewGame.score[i + 1][0] + NewGame.score[i + 1][1];
       NewGame.frames++;
